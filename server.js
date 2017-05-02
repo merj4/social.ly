55782441388c1fc5397243a2fb2626490415481d
Rochelle Valdez <rochellevee@gmail.com>
2017-05-02 00:17:48 -0700

    Initial set up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var bodyParser = require('body-parser')
 var path = require('path')
 var app = require('express')()
 var server = require('http').Server(app)
-var io = require('socket.io')(server)
+// var io = require('socket.io')(server)
 
 //Sockets
 var port = process.env.PORT || 8080
