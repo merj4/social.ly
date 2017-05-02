c50e0e3c8f5a9035567963e9b96f7de4b705044d
Rochelle Valdez <rochellevee@gmail.com>
2017-05-02 00:30:34 -0700

    Initial set up

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
-var app = require('express')()
-var server = require('http').Server(app)
-// var io = require('socket.io')(server)
+var app = express()
 
 //Sockets
 var port = process.env.PORT || 8080
-server.listen(port, () => {console.log('Listening on port ' + port)})
-// app.get('/', (req, res) => { res.sendFile(__dirname + '/public')})
+app.listen(port, () => {console.log('Listening on port ' + port)})
+app.get('/', (req, res) => { res.sendFile(__dirname + '/public')})
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}))
