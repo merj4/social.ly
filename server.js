92de9d5b3271bd8e22779e5ba4ac3ef70d0b7c86
Rochelle Valdez <rochellevee@gmail.com>
2017-05-02 09:15:36 -0700

    Database set up, heroku deploy, server set up, hello world successful

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ var express = require('express')
 var bodyParser = require('body-parser')
 var path = require('path')
 var app = express()
+var db = require('./server/db')
 
-//Sockets
 var port = process.env.PORT || 8080
 app.listen(port, () => {console.log('Listening on port ' + port)})
 app.get('/', (req, res) => { res.sendFile(__dirname + '/public')})
